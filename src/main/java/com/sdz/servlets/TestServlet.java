53724c352dfcfb7c940b903eaa1e54a2f72dd81d
BOUM Georges <37276156+GeorgesBOUM@users.noreply.github.com>
2022-02-15 18:47:42 +0100

    creation test servlet

---
+++ src/main/java/com/sdz/servlets/TestServlet.java
@@ -0,0 +1,12 @@
+package com.sdz.servlets;
+
+import javax.servlet.http.HttpServlet;
+
+public class TestServlet extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
